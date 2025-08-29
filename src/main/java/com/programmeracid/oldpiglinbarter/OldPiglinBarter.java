25cfa1e001e45eb7c00a2019e41fd9c5c3ac9c85
programmeracid <142600387+programmeracid@users.noreply.github.com>
2025-08-29 14:30:50 +0530

    Initial commit

---
+++ src/main/java/com/programmeracid/oldpiglinbarter/OldPiglinBarter.java
@@ -0,0 +1,17 @@
+package com.programmeracid.oldpiglinbarter;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OldPiglinBarter implements ModInitializer {
+	public static final String MOD_ID = "oldpiglinbarter";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/programmeracid/oldpiglinbarter/OldPiglinBarterClient.java
@@ -0,0 +1,10 @@
+package com.programmeracid.oldpiglinbarter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OldPiglinBarterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
